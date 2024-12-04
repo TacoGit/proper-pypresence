2356d5b28eaaab3bb1e99fd8c756b34b6b59d16f
tanos <68923270+TacoGit@users.noreply.github.com>
2024-12-04 18:55:53 +0100

    proper activities (watching/listening ... etc)

---
+++ pypresence/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pypresence',
+    version='3.5.1',
+    packages=find_packages(),
+    install_requires=[
+        'requests',  # Add any other dependencies the module needs
+    ],
+)
--- pypresence/utils.py
+++ pypresence/utils.py
@@ -5,7 +5,6 @@ import os
 import sys
 import tempfile
 import time
-import socket
 
 from .exceptions import PyPresenceException
 
@@ -22,25 +21,14 @@ def remove_none(d: dict):
     return d
 
 
-def test_ipc_path(path):
-    '''Tests an IPC pipe to ensure that it actually works'''
-    if sys.platform == 'win32' or sys.platform == 'win64':
-        with open(path):
-            return True
-    else:
-        with socket.socket(socket.AF_UNIX, socket.SOCK_STREAM) as client:
-            client.connect(path)
-            return True
-
-
 # Returns on first IPC pipe matching Discord's
 def get_ipc_path(pipe=None):
     ipc = 'discord-ipc-'
-    if pipe is not None:
+    if pipe:
         ipc = f"{ipc}{pipe}"
 
     if sys.platform in ('linux', 'darwin'):
-        tempdir = os.environ.get('XDG_RUNTIME_DIR') or (f"/run/user/{os.getuid()}" if os.path.exists(f"/run/user/{os.getuid()}") else tempfile.gettempdir())
+        tempdir = (os.environ.get('XDG_RUNTIME_DIR') or tempfile.gettempdir())
         paths = ['.', 'snap.discord', 'app/com.discordapp.Discord', 'app/com.discordapp.DiscordCanary']
     elif sys.platform == 'win32':
         tempdir = r'\\?\pipe'
@@ -52,7 +40,7 @@ def get_ipc_path(pipe=None):
         full_path = os.path.abspath(os.path.join(tempdir, path))
         if sys.platform == 'win32' or os.path.isdir(full_path):
             for entry in os.scandir(full_path):
-                if entry.name.startswith(ipc) and os.path.exists(entry) and test_ipc_path(entry.path):
+                if entry.name.startswith(ipc) and os.path.exists(entry):
                     return entry.path
 
 
